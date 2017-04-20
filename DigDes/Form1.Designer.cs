d1b95ceef57d4d1d66641dca71905c0dc767eb15
maria-elfimova <mira.elfimova@gmail.com>
2017-04-21 00:16:51 +0300

    image to text converter

---
+++ DigDes/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace DigDes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_convert = new System.Windows.Forms.Button();
+            this.label_path = new System.Windows.Forms.Label();
+            this.button_selectImage = new System.Windows.Forms.Button();
+            this.button_selectText = new System.Windows.Forms.Button();
+            this.label_pathToResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_convert
+            // 
+            this.button_convert.Location = new System.Drawing.Point(121, 105);
+            this.button_convert.Name = "button_convert";
+            this.button_convert.Size = new System.Drawing.Size(158, 23);
+            this.button_convert.TabIndex = 0;
+            this.button_convert.Text = "Convert";
+            this.button_convert.UseVisualStyleBackColor = true;
+            this.button_convert.Click += new System.EventHandler(this.button_convert_Click);
+            // 
+            // label_path
+            // 
+            this.label_path.AutoSize = true;
+            this.label_path.Location = new System.Drawing.Point(217, 34);
+            this.label_path.Name = "label_path";
+            this.label_path.Size = new System.Drawing.Size(0, 13);
+            this.label_path.TabIndex = 4;
+            // 
+            // button_selectImage
+            // 
+            this.button_selectImage.Location = new System.Drawing.Point(38, 29);
+            this.button_selectImage.Name = "button_selectImage";
+            this.button_selectImage.Size = new System.Drawing.Size(158, 23);
+            this.button_selectImage.TabIndex = 5;
+            this.button_selectImage.Text = "Select image";
+            this.button_selectImage.UseVisualStyleBackColor = true;
+            this.button_selectImage.Click += new System.EventHandler(this.button_selectImage_Click);
+            // 
+            // button_selectText
+            // 
+            this.button_selectText.Location = new System.Drawing.Point(38, 58);
+            this.button_selectText.Name = "button_selectText";
+            this.button_selectText.Size = new System.Drawing.Size(158, 27);
+            this.button_selectText.TabIndex = 6;
+            this.button_selectText.Text = "Select path to save the result ";
+            this.button_selectText.UseVisualStyleBackColor = true;
+            this.button_selectText.Click += new System.EventHandler(this.button_selectText_Click);
+            // 
+            // label_pathToResult
+            // 
+            this.label_pathToResult.AutoSize = true;
+            this.label_pathToResult.Location = new System.Drawing.Point(217, 65);
+            this.label_pathToResult.Name = "label_pathToResult";
+            this.label_pathToResult.Size = new System.Drawing.Size(0, 13);
+            this.label_pathToResult.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 140);
+            this.Controls.Add(this.label_pathToResult);
+            this.Controls.Add(this.button_selectText);
+            this.Controls.Add(this.button_selectImage);
+            this.Controls.Add(this.label_path);
+            this.Controls.Add(this.button_convert);
+            this.Name = "Form1";
+            this.Text = "Image to text converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_convert;
+        private System.Windows.Forms.Label label_path;
+        private System.Windows.Forms.Button button_selectImage;
+        private System.Windows.Forms.Button button_selectText;
+        private System.Windows.Forms.Label label_pathToResult;
+    }
+}
+
---
+++ DigDes/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DigDes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Bitmap image1;
+        private void button_convert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String path = label_path.Text;
+                image1 = new Bitmap(path);
+
+                FileInfo file = new FileInfo(label_pathToResult.Text);
+                StreamWriter stWr = file.CreateText();
+
+                int countWidth = image1.Width / 150;
+                int countHeight = image1.Height / 90;
+                Color pixelColor;
+                int color = 0;
+
+                for (int y = 0; y < image1.Height - countHeight + 1; y += countHeight)
+                {
+                    for (int x = 0; x < image1.Width - countWidth + 1; x += countWidth)
+                    {
+                        for (int p = 0; p < countWidth; p++)
+                        {
+                            for (int k = 0; k < countHeight; k++)
+                            {
+                                pixelColor = image1.GetPixel(x + p, y + k);
+                                color += (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
+                            }
+                        }
+                        color /= countWidth;
+                        color /= countHeight;
+                        if (color < 50)
+                        {
+                            stWr.Write("#");
+                        }
+                        else if (color < 100)
+                        {
+                            stWr.Write("%");
+                        }
+                        else if (color < 150)
+                        {
+                            stWr.Write("*");
+                        }
+                        else if (color < 200)
+                        {
+                            stWr.Write(":");
+                        }
+                        else if (color < 240)
+                        {
+                            stWr.Write(".");
+                        }
+                        else
+                        {
+                            stWr.Write(" ");
+                        }
+                    }
+                    stWr.WriteLine();
+                }
+                stWr.Close();
+                MessageBox.Show("Conversion completed.");
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("There was an error." +
+                    "Check the path to the image file.");
+            }
+        }
+
+        private void button_selectImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+                label_path.Text = openFileDialog.FileName;
+
+        }
+
+        private void button_selectText_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+                label_pathToResult.Text = openFileDialog.FileName;
+        }
+
+    }
+}
